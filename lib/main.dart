584fb785f1131276083eb6e3a129a81ebbba9870
Marcott <kasangandjomarcott47@gmail.com>
2024-10-24 14:53:49 +0200

    Right

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,19 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+// Future<void> loadRequest() async {
+//   try {
+//     final response = await http.get(Uri.parse("https://v1.udps.applipourtous.cd/dashboard"));
+//     if (response.statusCode == 200) {
+//       // Traitez la réponse ici
+//     } else {
+//       throw Exception('Erreur lors du chargement de l\'URL');
+//     }
+//   } catch (e) {
+//     print("Erreur: $e");
+//     }
+// }
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -38,33 +51,30 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     controller = WebViewController()
-      ..setJavaScriptMode(
-        JavaScriptMode.unrestricted,
-      )
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setNavigationDelegate(
         NavigationDelegate(
-          onProgress: (int progress) {},
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
           onPageStarted: (String url) {
             log("The Started Page:$url");
           },
           onPageFinished: (String url) {
-            log("The Finished Page : $url");
+            log("The Finished Page:$url");
           },
-          onWebResourceError: (WebResourceError erroe) {
-            log("Ther is some Erro Loading The Page");
-          },
+          onHttpError: (HttpResponseError error) {},
+          onWebResourceError: (WebResourceError error) {},
           onNavigationRequest: (NavigationRequest request) {
-            if (request.url.contains("udps")) {
-              log("Navigation is not valid");
+            if (request.url
+                .startsWith('https://v1.udps.applipourtous.cd/login/')) {
               return NavigationDecision.prevent;
             }
             return NavigationDecision.navigate;
           },
         ),
       )
-      ..loadRequest(
-        Uri.parse("http://10.10.0.238:8000/login"),
-      );
+      ..loadRequest(Uri.parse('https://v1.udps.applipourtous.cd/login'));
   }
 
   @override
